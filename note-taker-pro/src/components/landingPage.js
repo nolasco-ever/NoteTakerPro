b4e45aff47f64765f78e4f3ba916c471d4dead21
Ever Nolasco <nolasco.ever19@gmail.com>
2021-09-12 23:46:12 -0400

    Landing Page graphic
    Added image files for the landing page and created the graphics part of the landing page

--- note-taker-pro/src/components/landingPage.js
+++ note-taker-pro/src/components/landingPage.js
@@ -2,10 +2,25 @@ import React from 'react'
 import Header from './header'
 import Footer from './footer'
 
+import BusinessMeeting from './images/business-meeting.png'
+
 export default function LandingPage() {
     return (
         <div className="App">
             <Header/>
+            
+            <div className="landingPageContainer">
+                <div className="graphicsContainer">
+                    <div>
+                        <img src={BusinessMeeting}/>
+                        <p>A workspace just for you... sign up for free!</p>
+                    </div>
+                </div>
+
+                <div className="pageFormsContainer">
+                    
+                </div>
+            </div>
 
             <Footer/>
         </div>
