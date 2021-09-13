8b4dc5cbc25c8c65137d5d002fdb3598132e98db
Ever Nolasco <nolasco.ever19@gmail.com>
2021-09-13 00:59:30 -0400

    Form created

--- note-taker-pro/src/components/landingPage.js
+++ note-taker-pro/src/components/landingPage.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
+import { Form , Button, FloatingLabel} from 'react-bootstrap'
 
-import BusinessMeeting from './images/business-meeting.png'
+import BusinessMeeting from './images/Business-meeting.png'
 
 export default function LandingPage() {
     return (
@@ -18,7 +19,40 @@ export default function LandingPage() {
                 </div>
 
                 <div className="pageFormsContainer">
-                    
+                    <div className="buttonsContainer">
+                        <div className="visualSwitch"><p>Sign Up</p></div>
+                
+                        <button>Sign Up</button>
+                        <button>Login</button>
+                    </div>
+
+                    <div className="windowForm">
+                    <Form>
+                         <Form.Group className="textBox">
+                            <Form.Control type="text" placeholder="First Name" />
+                        </Form.Group>
+
+                        <Form.Group className="textBox">
+                            <Form.Control type="text" placeholder="Last Name" />
+                        </Form.Group>
+
+                        <Form.Group className="textBox" controlId="formBasicEmail">
+                            <Form.Control type="email" placeholder="Email Address" />
+                        </Form.Group>
+
+                        <Form.Group className="textBox" controlId="formBasicPassword">
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Form.Group className="textBox" controlId="formBasicPassword">
+                            <Form.Control type="password" placeholder="Confirm Password" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                    </div>
                 </div>
             </div>
 
